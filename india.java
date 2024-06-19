41e95cc920f510de097a9cb16d0f18602442060a
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-19 16:14:42 +0530

    .\india.java

---
+++ india.java
@@ -0,0 +1,23 @@
+package strings;
+import java.util.*;
+
+public class india {
+	public static void main(String []args)
+	{
+		String s1,s2;
+		Scanner shubham=new Scanner(System.in);
+		System.out.println("Enter one string ");
+		s1=shubham.nextLine();
+		s2="i love my contry india ";
+		if(s2.contains(s1))
+		{
+			System.out.println("world is present from the setence");
+		}
+		else 
+		{
+			System.out.println("world is not present");
+		}
+		
+	}
+
+}
