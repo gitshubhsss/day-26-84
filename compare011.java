baddc66b5312330174cd83ac52a0451cfe37b06d
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-19 16:13:12 +0530

    .\compare011.java

---
+++ compare011.java
@@ -0,0 +1,22 @@
+package strings;
+import java.util.*;
+public class compare011 {
+
+	public static void main(String[] args)
+	{
+		Scanner shubham=new Scanner(System.in);
+		System.out.println("Enter 2 string");
+		String s1=shubham.nextLine();
+		String s2=shubham.nextLine();
+		if(s1.equals(s2)==true)
+		{
+		System.out.println("both are equlas");
+		}
+		else
+		{
+		System.out.println("both are not equlas");
+		}
+
+	}
+
+}
